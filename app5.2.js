cb0d15a1552e706207772ddbf8530c078983e0f0
Nagaraju <nagaraju.kukudala@gmail.com>
2016-11-19 16:56:33 +0530

    Update app5.2.js

--- app5.2.js
+++ app5.2.js
@@ -1,5 +1,5 @@
 // object declaration for name, age, date of birth and place of birth  
-var details ={name:"Nagaraju Kukudala", age:25 , dateOfBirth:"20 Feb 1991",placeOfBirth:"Telangana"} 
+var details ={name:"Nagaraju Kukudala", age:25 , dateOfBirth:"20 Feb 1991",placeOfBirth:"Telangana"} ;
 
 
 myDetails();// Calling myDetails function
