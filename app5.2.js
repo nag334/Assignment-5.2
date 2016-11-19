a8d83746ce5aa35bfe899fa32601c2714a6010c2
Nagaraju <nagaraju.kukudala@gmail.com>
2016-11-19 17:11:39 +0530

    Update app5.2.js

--- app5.2.js
+++ app5.2.js
@@ -6,7 +6,7 @@ myDetails();// Calling myDetails function
 // function for Console.log
 function myDetails(){
 console.log("Name :" + details.name);  // prints Name on Console
-console.log("Age :" + details.age);     // prints Name on Console
+console.log("Age :" + details.age);     // prints Age on Console
 console.log("Date Of Birth :" + details.dateOfBirth);   // prints date of birth on Console
 console.log("Place Of Birth :" + details["placeOfBirth"]);  // prints place of birth on Console
     
